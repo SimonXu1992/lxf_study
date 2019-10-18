bdc0326608e879ab093cc9bd324f363df5fb44f6
SimonXu1992 <xsc19920226@163.com>
2019-10-18 22:13:40 +0800

    ThreadLocal的使用

--- ex/use_threadlocal.py
+++ ex/use_threadlocal.py
@@ -7,6 +7,9 @@ import threading
 
 ThreadLocal最常用的地方就是为每个线程绑定一个数据库连接，HTTP请求，用户身份信息等，这样一个线程的所有调用到的处理函数都可以非常方便地访问这些资源。
 """
+"""
+一个ThreadLocal变量虽然是全局变量，但每个线程都只能读写自己线程的独立副本，互不干扰。ThreadLocal解决了参数在一个线程中各个函数之间互相传递的问题。
+"""
 
 # 创建全局ThreadLocal对象
 local_school = threading.local()
